155d0f1b033bac17e815f6a8cec6bb1736bae156
AndreaPivetti <91748338+AndreaPivetti@users.noreply.github.com>
2021-10-05 15:12:48 +0200

    add an account creation system

--- src/WebCrawler/Accedi.java
+++ src/WebCrawler/Accedi.java
@@ -7,36 +7,51 @@ import java.util.Scanner;
 
 public class Accedi {
 
-	public void verificaPassword(String username) throws IOException, NoSuchAlgorithmException {
+	public void verificaPassword(String username, int registrazione) throws IOException, NoSuchAlgorithmException {
 
 		String password;
+		String hashedPass;
 		String[] account;
 		BufferedReader reader;
 		try {
 			File file = new File("auth.txt");
 			Scanner myReader = new Scanner(file);
-			while (myReader.hasNextLine()) {
-				String prelevaFile = myReader.nextLine();
-				if (prelevaFile.contains(":")) {
-					account = prelevaFile.split(":");
-					MessageDigest md = MessageDigest.getInstance("MD5");
-					md.update(account[1].getBytes());
-					String hashedPass = md.digest().toString().toUpperCase();
-					if (account[0].equals(username)) {
-						System.out.println("Inserisci la tua password: ");
-						reader = new BufferedReader(new InputStreamReader(System.in));
-						password = reader.readLine();
-						md = MessageDigest.getInstance("MD5");
-						md.update(password.getBytes());
-						hashedPass = md.digest().toString().toUpperCase();
-						while (!account[1].equals(hashedPass)) {
-							System.out.println("Password errata, riprova: ");
+			switch (registrazione) {
+			case 0:
+				while (myReader.hasNextLine()) {
+					String prelevaFile = myReader.nextLine();
+					if (prelevaFile.contains(":")) {
+						account = prelevaFile.split(":");
+						if (account[0].equals(username)) {
+							System.out.println("Inserisci la tua password: ");
 							reader = new BufferedReader(new InputStreamReader(System.in));
 							password = reader.readLine();
+							hashedPass = hashPassword(password);	
+							System.out.println(hashedPass);
+							while (!account[1].equals(hashedPass)) {
+								System.out.println("Password errata, riprova: ");
+								reader = new BufferedReader(new InputStreamReader(System.in));
+								password = reader.readLine();
+							}
+							System.out.println("Accesso effettuato ");
 						}
-						System.out.println("Accesso effettuato ");
 					}
 				}
+				break;
+			case 1:
+				try {
+                    FileWriter myWriter = new FileWriter("auth.txt", true);
+                    System.out.println("Inserisci una password: ");
+                    reader = new BufferedReader(new InputStreamReader(System.in));
+					password = reader.readLine();
+                    hashedPass = hashPassword(password);
+                    myWriter.write("\n" + username + ":" + hashedPass);
+                    myWriter.close();
+                } catch (IOException err) {
+                    System.out.println("Si è verificato un errore \n");
+                    err.printStackTrace();
+                }
+				break;
 			}
 		} catch (FileNotFoundException exc) {
 			System.out.println("Si è verificato un errore \n");
@@ -45,37 +60,36 @@ public class Accedi {
 
 	}
 
+	
+	public String hashPassword(String password) throws NoSuchAlgorithmException{
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		md.update(password.getBytes());
+		String hashedPass = md.digest().toString().toUpperCase();
+		return hashedPass;
+	}
+	
 	/*
-	public void hashPassword() throws NoSuchAlgorithmException {
-
-		String[] account;
-
-		try {
-			File file = new File("auth.txt");
-			Scanner myReader = new Scanner(file);
-			while (myReader.hasNextLine()) {
-				String prelevaFile = myReader.nextLine();
-				if (prelevaFile.contains(":")) {
-					account = prelevaFile.split(":");
-					MessageDigest md = MessageDigest.getInstance("MD5");
-					md.update(account[1].getBytes());
-					String hashedPass = md.digest().toString().toUpperCase();
-					System.out.println(account[1]);
-					System.out.println(hashedPass);
-					
-					 * try { FileWriter myWriter = new FileWriter("auth.txt", true);
-					 * myWriter.write(account[0] + ":" + hashedPass); myWriter.close();
-					 * System.out.println("Scrittura effettuata"); } catch (IOException err) {
-					 * System.out.println("Si è verificato un errore \n"); err.printStackTrace(); }
-					 
-				}
-
-			}
-		} catch (FileNotFoundException exc) {
-			System.out.println("Si è verificato un errore \n");
-			exc.printStackTrace();
-		}
-
-	}
-	*/
+	 * public void hashPassword() throws NoSuchAlgorithmException {
+	 * 
+	 * String[] account;
+	 * 
+	 * try { File file = new File("auth.txt"); Scanner myReader = new Scanner(file);
+	 * while (myReader.hasNextLine()) { String prelevaFile = myReader.nextLine(); if
+	 * (prelevaFile.contains(":")) { account = prelevaFile.split(":"); MessageDigest
+	 * md = MessageDigest.getInstance("MD5"); md.update(account[1].getBytes());
+	 * String hashedPass = md.digest().toString().toUpperCase();
+	 * System.out.println(account[1]); System.out.println(hashedPass);
+	 * 
+	 * try { FileWriter myWriter = new FileWriter("auth.txt", true);
+	 * myWriter.write(account[0] + ":" + hashedPass); myWriter.close();
+	 * System.out.println("Scrittura effettuata"); } catch (IOException err) {
+	 * System.out.println("Si è verificato un errore \n"); err.printStackTrace(); }
+	 * 
+	 * }
+	 * 
+	 * } } catch (FileNotFoundException exc) {
+	 * System.out.println("Si è verificato un errore \n"); exc.printStackTrace(); }
+	 * 
+	 * }
+	 */
 }
--- src/WebCrawler/WebCrawler.java
+++ src/WebCrawler/WebCrawler.java
@@ -7,15 +7,40 @@ public class WebCrawler {
 	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
 
 		String username;
+		int scelta;
+		int registrazione = 0;
 		Accedi accessoUtente = new Accedi();
+		
+		System.out.println("Cosa vuoi fare? \n" 
+							+ "1. Accedi; \n"
+							+ "2. Registrati; \n"
+							+ "3. Esci; ");
 
 		//accessoUtente.hashPassword();
-
-		System.out.println("Inserisci il tuo username: ");
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		username = reader.readLine();
+		scelta = Integer.valueOf(reader.readLine());
 
-		accessoUtente.verificaPassword(username);
-
+		switch (scelta){
+        case 1:
+        	System.out.println("Inserisci il tuo username: ");
+    		reader = new BufferedReader(new InputStreamReader(System.in));
+    		username = reader.readLine();
+    		accessoUtente.verificaPassword(username, registrazione);
+            break;
+        case 2:
+        	registrazione = 1;
+        	System.out.println("Qual è il tuo nome? ");
+        	reader = new BufferedReader(new InputStreamReader(System.in));
+    		username = reader.readLine();
+    		accessoUtente.verificaPassword(username, registrazione);
+            break;
+        case 3:
+        	System.out.println("Arrivederci!");
+            break;
+        default:
+        	System.out.println("Scelta non valida!");
+        	break;
+		}
+		
 	}
 }
