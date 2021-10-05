f7b52b251342a4c4feaa81c7ce7c9cf0d84dce7c
AndreaPivetti <91748338+AndreaPivetti@users.noreply.github.com>
2021-10-05 10:41:02 +0200

    modifica3

--- src/WebCrawler/Accedi.java
+++ src/WebCrawler/Accedi.java
@@ -6,62 +6,61 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
 public class Accedi {
-	
+
 	public void join(String username) throws IOException {
-		
-		
+
 		String password;
-		String[] account; 
+		String[] account;
 		BufferedReader reader;
 		try {
-            File file = new File("auth.txt");
-            Scanner myReader = new Scanner(file);
-            while (myReader.hasNextLine()) {
-                String prelevaFile = myReader.nextLine();
-                account = prelevaFile.split(":");
-                if(account[0].equals(username)) {
-                	System.out.println("Inserisci la tua password: ");
-                	reader = new BufferedReader(new InputStreamReader(System.in));
-            		password = reader.readLine();
-            		while(!password.equals(account[1])) {
-            			System.out.println("Password errata, riprova: ");
-            			reader = new BufferedReader(new InputStreamReader(System.in));
-                		password = reader.readLine();
-            		}
-            		System.out.println("Accesso effettuato ");
-                }
-            }
-        } catch (FileNotFoundException exc) {
-            System.out.println("Si è verificato un errore \n");
-            exc.printStackTrace();
-        }
-		
-	}
-	
-	public void hashPassword() throws NoSuchAlgorithmException {
-		
-		String hash = "35454B055CC325EA1AF2126E27707052";
-		String password = "ILoveJava";
-		String[] account;
-		
-		try {
-            File file = new File("auth.txt");
-            Scanner myReader = new Scanner(file);
-            while (myReader.hasNextLine()) {
-                String prelevaFile = myReader.nextLine();
-                account = prelevaFile.split(":");
-                MessageDigest md = MessageDigest.getInstance("MD5");
-                md.update(password.getBytes());
-                String hashedPass = new String( md.digest()).toUpperCase();
-                System.out.println(hash);
-                System.out.println(hashedPass);
-            }
-        } catch (FileNotFoundException exc) {
-            System.out.println("Si è verificato un errore \n");
-            exc.printStackTrace();
-        }
-		
+			File file = new File("auth.txt");
+			Scanner myReader = new Scanner(file);
+			while (myReader.hasNextLine()) {
+				String prelevaFile = myReader.nextLine();
+				account = prelevaFile.split(":");
+				if (account[0].equals(username)) {
+					System.out.println("Inserisci la tua password: ");
+					reader = new BufferedReader(new InputStreamReader(System.in));
+					password = reader.readLine();
+					while (!password.equals(account[1])) {
+						System.out.println("Password errata, riprova: ");
+						reader = new BufferedReader(new InputStreamReader(System.in));
+						password = reader.readLine();
+					}
+					System.out.println("Accesso effettuato ");
+				}
+			}
+		} catch (FileNotFoundException exc) {
+			System.out.println("Si è verificato un errore \n");
+			exc.printStackTrace();
+		}
 
 	}
-	
+
+	public void hashPassword() throws NoSuchAlgorithmException {
+
+		String[] account;
+
+		try {
+			File file = new File("auth.txt");
+			Scanner myReader = new Scanner(file);
+			while (myReader.hasNextLine()) {
+				String prelevaFile = myReader.nextLine();
+				if (prelevaFile.contains(":")) {
+					account = prelevaFile.split(":");
+					MessageDigest md = MessageDigest.getInstance("MD5");
+					md.update(account[1].getBytes());
+					String hashedPass = new String(md.digest()).toUpperCase();
+					System.out.println(account[1]);
+					System.out.println(hashedPass);
+				}
+
+			}
+		} catch (FileNotFoundException exc) {
+			System.out.println("Si è verificato un errore \n");
+			exc.printStackTrace();
+		}
+
+	}
+
 }
--- src/WebCrawler/WebCrawler.java
+++ src/WebCrawler/WebCrawler.java
@@ -1,24 +1,23 @@
 package WebCrawler;
 
-
 import java.io.*;
 import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
 public class WebCrawler {
-	public static void main(String[] args) throws IOException, NoSuchAlgorithmException{
-		
+	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+
 		String username;
 		String pass[] = new String[100];
 		Accedi acc = new Accedi();
-		
+
 		acc.hashPassword();
-		
+
 		System.out.println("Inserisci il tuo username: ");
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		username = reader.readLine();
-		
+
 		acc.join(username);
-		
+
 	}
 }
