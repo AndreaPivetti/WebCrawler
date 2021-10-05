ef0f75905ab1332fab9be28722c6bb9db6da517d
AndreaPivetti <91748338+AndreaPivetti@users.noreply.github.com>
2021-10-05 10:00:48 +0200

    modifica2

--- src/WebCrawler/Accedi.java
+++ src/WebCrawler/Accedi.java
@@ -1,11 +1,15 @@
 package WebCrawler;
 
 import java.io.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
 public class Accedi {
 	
 	public void join(String username) throws IOException {
+		
+		
 		String password;
 		String[] account; 
 		BufferedReader reader;
@@ -31,6 +35,33 @@ public class Accedi {
             System.out.println("Si è verificato un errore \n");
             exc.printStackTrace();
         }
+		
+	}
+	
+	public void hashPassword() throws NoSuchAlgorithmException {
+		
+		String hash = "35454B055CC325EA1AF2126E27707052";
+		String password = "ILoveJava";
+		String[] account;
+		
+		try {
+            File file = new File("auth.txt");
+            Scanner myReader = new Scanner(file);
+            while (myReader.hasNextLine()) {
+                String prelevaFile = myReader.nextLine();
+                account = prelevaFile.split(":");
+                MessageDigest md = MessageDigest.getInstance("MD5");
+                md.update(password.getBytes());
+                String hashedPass = new String( md.digest()).toUpperCase();
+                System.out.println(hash);
+                System.out.println(hashedPass);
+            }
+        } catch (FileNotFoundException exc) {
+            System.out.println("Si è verificato un errore \n");
+            exc.printStackTrace();
+        }
+		
+
 	}
 	
 }
--- src/WebCrawler/WebCrawler.java
+++ src/WebCrawler/WebCrawler.java
@@ -2,14 +2,18 @@ package WebCrawler;
 
 
 import java.io.*;
+import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
 public class WebCrawler {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException, NoSuchAlgorithmException{
 		
 		String username;
+		String pass[] = new String[100];
 		Accedi acc = new Accedi();
-				
+		
+		acc.hashPassword();
+		
 		System.out.println("Inserisci il tuo username: ");
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		username = reader.readLine();
